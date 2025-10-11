e1d3e96c688b5f346d03e8798c5d4545e9c7ea6d
Oktay <oktay@yatko.dev>
2025-10-11 14:31:09 +0200

    Improve profile card, use 3D skin renderer API

--- src/lib/components/hotkeys/profile.svelte
+++ src/lib/components/hotkeys/profile.svelte
@@ -8,17 +8,16 @@
 	}
 </script>
 
-<div class="flex h-full flex-col items-center justify-center gap-2 text-neutral-200">
-	<img
-		class="rounded-m size-8 xl:size-14"
-		src={`https://minotar.net/helm/${profileState.nickname}/100.png`}
-		alt={`${profileState.nickname}'s Minecraft profile picture`}
-	/>
-	<div class="text-sm xl:text-xl">
+<div
+	class="h-full bg-contain bg-position-[center_2.125rem] bg-no-repeat flex justify-center pt-1.5 overflow-hidden"
+	style={`background-image: url(https://nmsr.nickac.dev/bust/${profileState.nickname})`}
+>
+	<div class="text-sm xl:text-xl text-neutral-100">
 		<input
 			class="inline-block text-center"
 			type="text"
 			bind:value={nickname}
+			defaultValue={nickname}
 			onblur={commitNickname}
 			onkeydown={(e) => e.key === 'Enter' && commitNickname()}
 		/>
